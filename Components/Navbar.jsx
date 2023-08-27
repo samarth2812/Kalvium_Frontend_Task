5bff0d918cbd5c709f4060964a27ce65bbf78ca3
Samarth <samarthdhawan28@gmail.com>
2023-08-27 13:07:19 +0530

    4-components-ready

---
+++ Components/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import styles from '@/styles/Navbar.module.css'
+import Link from 'next/link'
+
+const Navbar = () => {
+  return (
+    <>
+    <div>
+        <nav className={styles.mainnav}>
+        <ul className={styles.hello}>
+             <h2 className={styles.fo}>Hi 👋, Samarth</h2>
+            </ul>
+            <ul className={styles.hello}>
+                 <Link className={styles.u} legacyBehavior href='/'><a className={styles.fer}><li>Home</li></a></Link>
+                 <Link className={styles.u} legacyBehavior href='/'><a className={styles.fer}><li> </li>Subject-wise attendence</a></Link> 
+                <Link className={styles.u} legacyBehavior href='/Project'><a className={styles.fer}><li>Timetable</li></a></Link>
+                <Link className={styles.u} legacyBehavior href='/attendence'><a className={styles.fer}><li>Mark your attendence</li></a></Link> 
+            </ul>
+
+        </nav> 
+      
+    </div>
+    </>
+  )
+}
+
+export default Navbar
